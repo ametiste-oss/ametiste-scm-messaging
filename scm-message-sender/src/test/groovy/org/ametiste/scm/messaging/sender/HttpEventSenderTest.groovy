2a7dc9b76420b878092942952442ce3e191439ad
abliznyuk <bliznyuk.andrey@gmail.com>
2015-11-11 01:46:51 +0200

    sender tests

--- scm-message-sender/src/test/groovy/org/ametiste/scm/messaging/sender/HttpEventSenderTest.groovy
+++ scm-message-sender/src/test/groovy/org/ametiste/scm/messaging/sender/HttpEventSenderTest.groovy
@@ -5,6 +5,8 @@ import org.ametiste.scm.messaging.data.event.InstanceLifecycleEvent
 import org.ametiste.scm.messaging.data.transport.TransportMessage
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.ametiste.scm.messaging.transport.http.dto.factory.DefaultEventToConverterMapFactory
+import org.ametiste.scm.messaging.transport.http.dto.factory.EventDTOFactory
 import org.apache.http.client.HttpClient
 import org.apache.http.client.ResponseHandler
 import org.apache.http.client.methods.HttpPost
@@ -14,6 +16,7 @@ import spock.lang.Specification
 
 import java.util.stream.Collectors
 
+import static java.util.Collections.singletonList
 import static org.ametiste.scm.messaging.data.event.InstanceLifecycleEvent.Type.SHUTDOWN
 import static org.ametiste.scm.messaging.data.event.InstanceLifecycleEvent.Type.STARTUP
 
@@ -32,6 +35,26 @@ class HttpEventSenderTest extends Specification {
         events = new ArrayList<>();
         events.add(InstanceLifecycleEvent.builder().type(STARTUP).instanceId("DSF").version("0.2.6").build());
         events.add(InstanceLifecycleEvent.builder().type(SHUTDOWN).instanceId("DIE").version("1.0.5").build());
+    }
+
+    def "initialization checks"() {
+        when: "try create sender with not initialized http client"
+        new HttpEventSender(null, )
+
+        then: "expect IllegalArgumentException thrown"
+        thrown(IllegalArgumentException.class)
+
+        when: "try create sender with not initialized converter map"
+        new HttpEventSender(client, null)
+
+        then: "expect IllegalArgumentException thrown"
+        thrown(IllegalArgumentException.class)
+
+        when: "try create sender with correct parameters"
+        new HttpEventSender(client, new DefaultEventToConverterMapFactory().getMap())
+
+        then: "any exception not thrown"
+        notThrown(IllegalArgumentException.class)
     }
 
     def "sender should send correct event request"() {
@@ -66,5 +89,53 @@ class HttpEventSenderTest extends Specification {
             assert json.isArray()
             assert json.elements().size() == 2
         }
+    }
+
+    def "sender shouldn't send empty pack of messages"() {
+        given: "list of messages with target in excludes"
+        List<TransportMessage<Event>> messages = [new TransportMessage<Event>(events[0], singletonList(target)),
+                                                new TransportMessage<Event>(events[1], singletonList(target))]
+
+        when: "send messages to target"
+        sender.send(target, messages);
+
+        then: "client not invoked"
+        0 * client.execute(_, _)
+    }
+
+    def "create http client with correct parameters"() {
+        given: "timeouts"
+        def readTimeout = 1000;
+        def connectTimeout = 300;
+
+        when: "create client with with parameters"
+        CloseableHttpClient client = HttpEventSender.createHttpClient(connectTimeout, readTimeout)
+
+        then: "expect client is correct"
+        client != null
+    }
+
+    def "sender exception on client error"() {
+        when: "send messages and client throw exception"
+        sender.send(target, new TransportMessage<Event>(events[0]))
+        client.execute(_, _) >> { throw new IOException(); }
+
+        then: "expect exception intercepted and EventSendException thrown"
+        thrown(EventSendException.class)
+    }
+
+    def "sender exception on message serialising"() {
+
+    }
+
+    def "sender exception on converter missing"() {
+        given: "sender with empty converters map"
+        HttpEventSender eventSender = new HttpEventSender(client, Collections.emptyMap())
+
+        when: "try send message"
+        eventSender.send(target, new TransportMessage<Event>(events[0]))
+
+        then: "expect EventSendException thrown"
+        thrown(EventSendException.class)
     }
 }
