92f052b9388f46e1b226069fc1abad830490a3dd
abliznyuk <abliznyuk>
2015-11-12 11:59:41 +0200

    move config to starter

--- scm-message-sender-client/src/test/groovy/org/ametiste/scm/messaging/config/AppPropertiesAggregatorTestConfiguration.java
+++ scm-message-sender-client/src/test/groovy/org/ametiste/scm/messaging/config/AppPropertiesAggregatorTestConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-//@EnableAutoConfiguration
 @PropertySource("classpath:test-props-aggregator.properties")
 public class AppPropertiesAggregatorTestConfiguration {
 
